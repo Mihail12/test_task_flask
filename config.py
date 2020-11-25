69693cc2e0f17b88764660d1a5cb9e0f84aedeeb
mihail12 <spasenkomihail@gmail.com>
2020-11-25 15:00:22 +0200

    remove useless string

--- config.py
+++ config.py
@@ -7,16 +7,6 @@ logger = logging.getLogger(__name__)
 class DevelopmentConfig(object):
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-    DATABASES = {
-        "default": {
-            "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-            "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-            "USER": os.environ.get("SQL_USER", "user"),
-            "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-            "HOST": os.environ.get("SQL_HOST", "localhost"),
-            "PORT": os.environ.get("SQL_PORT", "5432"),
-        }
-    }
     db_engine = os.environ.get("SQL_ENGINE", 'sqlite3')
     db_user = os.environ.get("SQL_USER", 'user')
     db_password = os.environ.get("SQL_PASSWORD", 'password')
